6369efd3eaa6065b87a9833bb3504e8e72e4191a
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-12-06 18:50:26 +0100

    just commit - hotel scraper for 5 hotels

--- index.js
+++ index.js
@@ -399,189 +399,189 @@ const wymara_resort = async (
   return result;
 };
 
-// const cancun = async (
-//   arrival_date = "2024-01-13",
-//   departure_Date = "2024-01-20"
-// ) => {
-//   let result = await (async (arrival_date, departure_Date) => {
-//     // Launch the browser and open a new blank page
-//     const oldProxyUrl =
-//       "http://webmaster_ultimatejetvacations_com-country-us:fr8xz368j0@gate.nodemaven.com:8080";
-//     const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-//     // console.log({ newProxyUrl });
-//     let browser = await chromium.puppeteer.launch({
-//       args: [`--proxy-server=${newProxyUrl}`],
-//       defaultViewport: chromium.defaultViewport,
-//       executablePath: await chromium.executablePath,
-//       headless: true,
-//       ignoreHTTPSErrors: true,
-//     });
+const cancun = async (
+  arrival_date = "2024-01-13",
+  departure_Date = "2024-01-20"
+) => {
+  let result = await (async (arrival_date, departure_Date) => {
+    // Launch the browser and open a new blank page
+    const oldProxyUrl =
+      "http://webmaster_ultimatejetvacations_com-country-us:fr8xz368j0@gate.nodemaven.com:8080";
+    const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+    // console.log({ newProxyUrl });
+    let browser = await chromium.puppeteer.launch({
+      args: [`--proxy-server=${newProxyUrl}`],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
 
-//     //configuration
-//     const page = await browser.newPage();
-//     await page.setViewport({ width: 0, height: 0 });
-//     await page.setDefaultNavigationTimeout(120000);
+    //configuration
+    const page = await browser.newPage();
+    await page.setViewport({ width: 0, height: 0 });
+    await page.setDefaultNavigationTimeout(120000);
 
-//     let arrival = arrival_date;
-//     let departure = departure_Date;
-//     let [year, mth, day] = arrival.split("-");
-//     let [yearD, mthD, dayD] = departure.split("-");
-//     mth = mth[0] == "0" ? mth[1] : mth;
-//     mthD = mthD[0] == "0" ? mthD[1] : mthD;
-//     day = day[0] == "0" ? day[1] : day;
-//     dayD = dayD[0] == "0" ? dayD[1] : dayD; // in cases of days with singular values
+    // let arrival = arrival_date;
+    // let departure = departure_Date;
+    // let [year, mth, day] = arrival.split("-");
+    // let [yearD, mthD, dayD] = departure.split("-");
+    // mth = mth[0] == "0" ? mth[1] : mth;
+    // mthD = mthD[0] == "0" ? mthD[1] : mthD;
+    // day = day[0] == "0" ? day[1] : day;
+    // dayD = dayD[0] == "0" ? dayD[1] : dayD; // in cases of days with singular values
 
-//     let website_url = `https://bookings-cancun.garzablancaresort.com/en/bookcore/availability/rooms/gblancacancun/`;
+    let website_url = `https://bookings-cancun.garzablancaresort.com/en/bookcore/availability/rooms/gblancacancun/`;
 
-//     // Navigate the page to a URL
-//     await page.goto(website_url);
+    // Navigate the page to a URL
+    await page.goto(website_url);
 
-//     await page.click(
-//       "#roi-search-engine > form > div.roi-search-engine__item.roi-search-engine__item--dates > button"
-//     ),
-//       await page.waitForSelector("#roicalendar");
+    await page.click(
+      "#roi-search-engine > form > div.roi-search-engine__item.roi-search-engine__item--dates > button"
+    ),
+      await page.waitForSelector("#roicalendar");
 
-//     // arrival select
-//     const arrivalSelect = await page.$$(
-//       `td.roi-cal__day.js-calendar-day[data-day='${day}'][data-month='${
-//         mth - 1
-//       }'][data-year='${year}']:not([data-weekday='null'])`
-//     );
+    // arrival select
+    const arrivalSelect = await page.$$(
+      `td.roi-cal__day.js-calendar-day[data-day='${day}'][data-month='${
+        mth - 1
+      }'][data-year='${year}']:not([data-weekday='null'])`
+    );
 
-//     if (arrivalSelect.length > 0) {
-//       await arrivalSelect[0].click();
-//     } else {
-//       console.log("No matching element found");
-//     }
+    if (arrivalSelect.length > 0) {
+      await arrivalSelect[0].click();
+    } else {
+      console.log("No matching element found");
+    }
 
-//     // departure select
-//     const departureSelect = await page.$$(
-//       `td.roi-cal__day.js-calendar-day[data-day='${dayD}'][data-month='${
-//         mthD - 1
-//       }'][data-year='${yearD}']:not([data-weekday='null'])`
-//     );
+    // departure select
+    const departureSelect = await page.$$(
+      `td.roi-cal__day.js-calendar-day[data-day='${dayD}'][data-month='${
+        mthD - 1
+      }'][data-year='${yearD}']:not([data-weekday='null'])`
+    );
 
-//     if (departureSelect.length > 0) {
-//       await departureSelect[0].click();
-//     } else {
-//       console.log("No matching element found");
-//     }
+    if (departureSelect.length > 0) {
+      await departureSelect[0].click();
+    } else {
+      console.log("No matching element found");
+    }
 
-//     //click 'select dates'
-//     await page.click(".roi-cal__close.js-calendar-close");
-//     // await Promise.all([
-//     //   page.waitForNavigation(),
-//     //   page.click(".roi-search-engine__field.roi-search-engine__field--action"),
-//     // ]);
-//     await page.click(
-//       ".roi-search-engine__field.roi-search-engine__field--action"
-//     );
+    //click 'select dates'
+    await page.click(".roi-cal__close.js-calendar-close");
+    // await Promise.all([
+    //   page.waitForNavigation(),
+    //   page.click(".roi-search-engine__field.roi-search-engine__field--action"),
+    // ]);
+    await page.click(
+      ".roi-search-engine__field.roi-search-engine__field--action"
+    );
 
-//     // await page.waitForNavigation();
-//     await new Promise((resolve) => setTimeout(resolve, 50000));
+    // await page.waitForNavigation();
+    await new Promise((resolve) => setTimeout(resolve, 50000));
 
-//     // to check if we have or do not have a result
-//     let roomVal = await page.evaluate(() => {
-//       let el = document.querySelector(
-//         "#hoteles_sin_disponiblidad > div.no_dispo_titulo_wrap"
-//       );
-//       return el ? el.innerText : "";
-//     });
+    // to check if we have or do not have a result
+    let roomVal = await page.evaluate(() => {
+      let el = document.querySelector(
+        "#hoteles_sin_disponiblidad > div.no_dispo_titulo_wrap"
+      );
+      return el ? el.innerText : "";
+    });
 
-//     const all_rooms = await page.evaluate(() =>
-//       Array.from(document?.querySelectorAll(".habitacion"), (e) => ({
-//         room_name: e?.querySelector("h3.hab_titulo")?.innerText,
-//         rates_1: Array.from(
-//           e?.querySelectorAll(
-//             // ".contenedor-precio.multiprecio.precio-total.roi-boards-total-price__amount"
-//             ".roi-boards__block.roi-boards__block--average-price"
-//           ),
-//           (ev) => ev?.innerText
-//         ).slice(1, 3),
-//         rates_2: Array.from(
-//           e?.querySelectorAll(
-//             ".contenedor-precio.multiprecio.precio-total.roi-boards-total-price__amount"
-//           ),
-//           (ev) => ev?.innerText
-//         ).slice(0, 2),
-//         // sleeps_bed_size: e?.querySelector(".thumb-cards_urgencyTriggerAndRoomInfo")
-//         //   .innerText,
-//         description: e
-//           ?.querySelector(
-//             "div.hab_head > div.hab_head_info.col-lg-8.col-md-8.col-sm-8.col-xs-12"
-//           )
-//           ?.innerText.replace("\n", "")
-//           .replace("See more", ""),
-//       }))
-//     );
+    const all_rooms = await page.evaluate(() =>
+      Array.from(document?.querySelectorAll(".habitacion"), (e) => ({
+        room_name: e?.querySelector("h3.hab_titulo")?.innerText,
+        rates_1: Array.from(
+          e?.querySelectorAll(
+            // ".contenedor-precio.multiprecio.precio-total.roi-boards-total-price__amount"
+            ".roi-boards__block.roi-boards__block--average-price"
+          ),
+          (ev) => ev?.innerText
+        ).slice(1, 3),
+        rates_2: Array.from(
+          e?.querySelectorAll(
+            ".contenedor-precio.multiprecio.precio-total.roi-boards-total-price__amount"
+          ),
+          (ev) => ev?.innerText
+        ).slice(0, 2),
+        // sleeps_bed_size: e?.querySelector(".thumb-cards_urgencyTriggerAndRoomInfo")
+        //   .innerText,
+        description: e
+          ?.querySelector(
+            "div.hab_head > div.hab_head_info.col-lg-8.col-md-8.col-sm-8.col-xs-12"
+          )
+          ?.innerText.replace("\n", "")
+          .replace("See more", ""),
+      }))
+    );
 
-//     // modify allRooms
-//     const allRooms = [];
+    // modify allRooms
+    const allRooms = [];
 
-//     for (i = 0; i < all_rooms.length; i++) {
-//       const { room_name, rates_1, rates_2, description } = all_rooms[i];
-//       let errMsg = "not available";
-//       let defaultCurrency = "US$";
-//       allRooms.push({
-//         room_name,
-//         description,
-//         rates: [
-//           {
-//             rate_code: "room_only",
-//             rate_description: "AVERAGE PRICE/NIGHT",
-//             price: rates_1.length > 0 ? rates_1[0].split(" ")[1] : errMsg,
-//             currency:
-//               rates_1.length > 0 ? rates_1[0].split(" ")[0] : defaultCurrency,
-//           },
-//           {
-//             rate_code: "all_inclusive",
-//             rate_description: "AVERAGE PRICE/NIGHT",
-//             price: rates_1.length > 0 ? rates_1[1].split(" ")[1] : errMsg,
-//             currency:
-//               rates_1.length > 0 ? rates_1[1].split(" ")[0] : defaultCurrency,
-//           },
-//           {
-//             rate_code: "all_inclusive",
-//             rate_description: "TOTAL",
-//             price: rates_2.length > 0 ? rates_2[1].split(" ")[1] : errMsg,
-//             currency:
-//               rates_2.length > 0 ? rates_2[1].split(" ")[0] : defaultCurrency,
-//           },
-//           {
-//             rate_code: "room_only",
-//             rate_description: "TOTAL",
-//             price: rates_2.length > 0 ? rates_2[0].split(" ")[1] : errMsg,
-//             currency:
-//               rates_2.length > 0 ? rates_2[0].split(" ")[0] : defaultCurrency,
-//           },
-//         ],
-//       });
-//     }
+    for (i = 0; i < all_rooms.length; i++) {
+      const { room_name, rates_1, rates_2, description } = all_rooms[i];
+      let errMsg = "not available";
+      let defaultCurrency = "US$";
+      allRooms.push({
+        room_name,
+        description,
+        rates: [
+          {
+            rate_code: "room_only",
+            rate_description: "AVERAGE PRICE/NIGHT",
+            price: rates_1.length > 0 ? rates_1[0].split(" ")[1] : errMsg,
+            currency:
+              rates_1.length > 0 ? rates_1[0].split(" ")[0] : defaultCurrency,
+          },
+          {
+            rate_code: "all_inclusive",
+            rate_description: "AVERAGE PRICE/NIGHT",
+            price: rates_1.length > 0 ? rates_1[1].split(" ")[1] : errMsg,
+            currency:
+              rates_1.length > 0 ? rates_1[1].split(" ")[0] : defaultCurrency,
+          },
+          {
+            rate_code: "all_inclusive",
+            rate_description: "TOTAL",
+            price: rates_2.length > 0 ? rates_2[1].split(" ")[1] : errMsg,
+            currency:
+              rates_2.length > 0 ? rates_2[1].split(" ")[0] : defaultCurrency,
+          },
+          {
+            rate_code: "room_only",
+            rate_description: "TOTAL",
+            price: rates_2.length > 0 ? rates_2[0].split(" ")[1] : errMsg,
+            currency:
+              rates_2.length > 0 ? rates_2[0].split(" ")[0] : defaultCurrency,
+          },
+        ],
+      });
+    }
 
-//     if (allRooms.length > 0) {
-//       console.log({ status: "sucess", res: allRooms });
-//     } else {
-//       console.log({
-//         status: "error",
-//         res: "Could not fetch page",
-//         websiteErrorMsg: roomVal,
-//       });
-//     }
+    if (allRooms.length > 0) {
+      console.log({ status: "sucess", res: allRooms });
+    } else {
+      console.log({
+        status: "error",
+        res: "Could not fetch page",
+        websiteErrorMsg: roomVal,
+      });
+    }
 
-//     // Type into search box
-//     let title = await page.title();
+    // Type into search box
+    let title = await page.title();
 
-//     // console.log(title);
+    // console.log(title);
 
-//     // await process.exit();
-//     await browser.close();
-//     let response = { rooms: allRooms, title };
-//     return roomVal ? { res: roomVal } : response;
-//   })(arrival_date, departure_Date);
-//   // console.log(result);
+    // await process.exit();
+    await browser.close();
+    let response = { rooms: allRooms, title };
+    return roomVal ? { res: roomVal } : response;
+  })(arrival_date, departure_Date);
+  // console.log(result);
 
-//   return result;
-// };
+  return result;
+};
 
 // // sugar_beach("2024-01-17", "2024-01-25"); // excluded by client
 // // cancun("2024-01-17", "2024-01-25"); // works perfectly in both modes
